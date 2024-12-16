d35a6635b97274600ffe828b8040eecdae247d76
mirkodzudzar <mirkodzudzar@gmail.com>
2024-12-16 09:51:20 +0100

    docker file changes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
 
+# Composer install
 if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-progress --no-interaction
 fi
 
+# Environment file
 if [ ! -f ".env" ]; then
     echo "Creating env file for env $APP_ENV"
     cp .env.example .env
@@ -11,10 +14,12 @@ else
     echo "env file exists."
 fi
 
+# Determine container role
 role=${CONTAINER_ROLE:-app}
 
 if [ "$role" = "app" ]; then
-    php artisan migrate
+    # Application server tasks
+    php artisan migrate --force
     php artisan key:generate
     php artisan cache:clear
     php artisan config:clear
@@ -22,12 +27,14 @@ if [ "$role" = "app" ]; then
     php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
     exec docker-php-entrypoint
 elif [ "$role" = "queue" ]; then
+    # Queue worker tasks
     echo "Waiting for the database to be available..."
+    # Use -z for zero-IO mode, -v for verbose, -w for timeout
     until nc -z -v -w30 database 3306; do
         echo "Waiting for MySQL to be available..."
         sleep 5
     done
 
     echo "Database is up. Running the queue..."
-    php artisan queue:work --verbose --tries=3 --timeout=1
+    php artisan queue:work --verbose --tries=3 --timeout=90
 fi
